e6e5da8388fb74022ce5fb8728fea256f4471738
emin4454 <132940987+emin4454@users.noreply.github.com>
2023-12-15 00:37:53 +0300

    Processes alive time feature imported

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -6,11 +6,13 @@ public abstract class Scheduler {
     protected final Queue<ExecutableProcess> readyQueue = new LinkedList<>(); // Prosesler icin hazir kuyrugu
     protected int priorityLevel;
     protected Device device;
+
     public abstract void executeOneIteration(Scheduler[] schedulers);
 
-    Scheduler(Device device){
+    Scheduler(Device device) {
         this.device = device;
     }
+
     public void addToQueue(ExecutableProcess process) {
         readyQueue.add(process);
     }
@@ -18,8 +20,26 @@ public abstract class Scheduler {
     public boolean isListEmpty() {
         return readyQueue.isEmpty();
     }
+
     protected void printSchedulerInfo() {
         String sch = priorityLevel == 0 ? "Gercek Zamanli (FCFS) " : priorityLevel + ". Seviye Geri Beslemeli(Round Robin)";
         System.out.println("Executelanan Prosesin Gorevlendirici Duzeyi: " + sch);
+    }
+
+    public void increaseAliveTimeAllQueue(Queue<ExecutableProcess> timeOutList) {
+        if (!isListEmpty()) {
+            ExecutableProcess firstItem = readyQueue.poll();
+            firstItem.increaseAliveTime();
+            readyQueue.add(firstItem);
+            while (firstItem != readyQueue.peek()) {
+                ExecutableProcess item = readyQueue.poll();
+                if(item.increaseAliveTime()){
+                    timeOutList.add(item);
+                }
+                else {
+                    readyQueue.add(item);
+                }
+            }
+        }
     }
 }
